7258c589aa3e2ee080a47199ae601a13cfc86da3
Mark Gatongay <antoniogatongay@Royal-Rikus-MacBook-Pro.local>
2019-06-13 12:39:08 -0400

    final commit to main.py script

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,15 +15,16 @@ with open(budgetpath,'r') as budget_handler:
 
     for row in budgetcsv:
         change = int(row[1])
+        date = row[0]
         # Second task
         total += change
         row_list.append(row)
         # Fourth task
         if change > greatest[0]:
-            greatest = (change, len(row_list) - 1)
+            greatest = (change, date)
         # Fifth task
         if change < least[0]:
-            least = (change, len(row_list) - 1)
+            least = (change, date)
 
 # First task
 total_months = len(row_list)
@@ -31,10 +32,15 @@ total_months = len(row_list)
 avg_change = round(total/total_months, 2)
 
 # Results
-print(total_months)
-print(total)
-print(avg_change)
-print(greatest)
-print(least)
-print(row_list[greatest[1]])
-print(row_list[least[1]])
+Results = ['Financial Analysis', '----------------------------', 
+f'Total Months: {total_months}',
+f'Total: ${total}', f'Average Change: ${avg_change}', 
+f'Greatest Increase in Profits: {greatest[1]} (${greatest[0]})',
+f'Greatest Decrease in Profits: {least[1]} (${least[0]})']
+
+with open('analysis.txt', 'w') as written:
+    writer = csv.writer(written)
+    writer.writerows([row_write] for row_write in  Results)
+
+for line in Results:
+    print(line)
