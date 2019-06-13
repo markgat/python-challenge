f76a67cebab9340e56bd0c987046c6bd95655e66
Mark Gatongay <antoniogatongay@Royal-Rikus-MacBook-Pro.local>
2019-06-13 08:46:19 -0400

    General main.py code before formatting / exporting

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,40 @@
 import csv
 import os
 
-budgetpath = os.path.join('..', 'Resources', 'budget_data.csv')
+row_list = []
+total = 0
+greatest = (0, None)
+least = (0, None)
+
+budgetpath = os.path.join('Resources', 'budget_data.csv')
 
 with open(budgetpath,'r') as budget_handler:
-    budgetrows = csv.reader(budget_handler, delimiter=',')
+    budgetcsv = csv.reader(budget_handler, delimiter=',')
 
-    header = next(budgetrows)
+    header = next(budgetcsv)
+
+    for row in budgetcsv:
+        change = int(row[1])
+        # Second task
+        total += change
+        row_list.append(row)
+        # Fourth task
+        if change > greatest[0]:
+            greatest = (change, len(row_list) - 1)
+        # Fifth task
+        if change < least[0]:
+            least = (change, len(row_list) - 1)
+
+# First task
+total_months = len(row_list)
+# Third task 
+avg_change = round(total/total_months, 2)
+
+# Results
+print(total_months)
+print(total)
+print(avg_change)
+print(greatest)
+print(least)
+print(row_list[greatest[1]])
+print(row_list[least[1]])
